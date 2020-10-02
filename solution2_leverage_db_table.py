746656b110bd9fd2db6134da9b9533d7320bfbd7
Erik Yan <13530393+eyan02@users.noreply.github.com>
2020-10-02 07:53:58 -0700

    Update solution2_leverage_db_table.py

--- solution2_leverage_db_table.py
+++ solution2_leverage_db_table.py
@@ -1,22 +1,25 @@
 #TODO: add sample
-# Table query is dependent on database, and in this example we leverage a static dictionary to execute our functions (to avoid security holes via function injects).
+# Table query is dependent on database.
+#In this example we leverage a Django table and static dictionary to execute our functions
+#Static dictionary ensures only authorized functions may be called from database to avoid compromising code injections).
 
-dynamic = [component_type==None, sub_component==None, title==None]
-if dynamic == [False,False,False]:
-    result = Table.objects.filter(
-        component_type__exact = f'{component_type}',
-        sub_component_type__exact = f'{sub_component}',
-        title__exact = f'{title}')
-elif dynamic == [False,False,True]:
-    result = Table.objects.filter(
-        component_type__exact = f'{component_type}',
-        sub_component_type__exact = f'{sub_component}')
-elif dynamic == [False,True,False]:
-    result = Table.objects.filter(
-        component_type__exact = f'{component_type}',
-        title__exact = f'{title}')
-elif dynamic == [False,True,True]:
-    result = CardsStatistics.objects.filter(
-        component_type__exact = f'{component_type}')
-else:
-    result = None
+database_query(part,component,sub_component):
+    dynamic = [part==None, component==None, sub_component==None]
+    if dynamic == [False,False,False]:
+        result = Table.objects.filter(
+            part__exact = f'{part}',
+            component__exact = f'{component}',
+            title__exact = f'{sub_component}')
+    elif dynamic == [False,False,True]:
+        result = Table.objects.filter(
+            part__exact = f'{part}',
+            component__exact = f'{component}')
+    elif dynamic == [False,True,False]:
+        result = Table.objects.filter(
+            part__exact = f'{part}',
+            sub_component__exact = f'{sub_component}')
+    elif dynamic == [False,True,True]:
+        result = CardsStatistics.objects.filter(
+            part__exact = f'{part}')
+    else:
+        result = None
